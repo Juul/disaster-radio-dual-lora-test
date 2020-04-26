503186772c98e37328ad6f9897a4c4781c32ec0c
Marc Juul <marc@juul.io>
2020-04-26 07:26:15 -0700

    switched to radiolib

--- README.md
+++ README.md
@@ -9,14 +9,14 @@ Go to `File -> Options` then click the button to the right of `Additional Boards
 
 Go to `Tools -> Board -> Boards Manager` then search for "esp32" and install the only result.
 
-Go to `Sketch -> Include Library -> Manage Libraries` then search for "lora" and install the one called "LoRa by Sandeep Mistry".
+Go to `Sketch -> Include Library -> Manage Libraries` then search for "radiolib" and install the only result.
 
 Now open `main.ino` from this repository.
 
 In `Tools` select the following:
 
 * Board: ESP32 Dev Module
-* Upload speed: Upload speed 115200
+* Upload speed: Upload speed 921600
 * CPU Frequency: 80 MHz
 * Flash mode: DIO
 * Flash size; 4 MB
@@ -27,3 +27,10 @@ Hook the board to 3.3 V and USB.
 Now you should be able to upload.
 
 Go to `Tools -> Serial Monitor` to see the output after uploading.
+
+To switch to the second LoRa module, comment out the first line and uncomment the second line just before `setup()`:
+
+```
+SX1262 lora = new Module(ss_lora1, dio2_lora1, rst, busy_lora1);
+//SX1262 lora = new Module(ss_lora2, dio2_lora2, rst, busy_lora2);
+```
--- main.ino
+++ main.ino
@@ -1,48 +1,112 @@
-#include <SPI.h>
-#include <LoRa.h>
+/*
+   RadioLib SX126x Transmit Example
+
+   This example transmits packets using SX1262 LoRa radio module.
+   Each packet contains up to 256 bytes of data, in the form of:
+    - Arduino String
+    - null-terminated char array (C-string)
+    - arbitrary binary data (byte array)
+
+   Other modules from SX126x family can also be used.
+
+   For full API reference, see the GitHub Pages
+   https://jgromes.github.io/RadioLib/
+*/
+
+// include the library
+#include <RadioLib.h>
+
 
 #define ss_lora1 16 // SS pin for 1st LoRa module
 #define ss_lora2 34 // SS pin for 2nd LoRa module
 #define rst 5 // setting to ESP LED since we don't have reset
-//#define dio0 21
+#define busy_lora1 22
+#define busy_lora2 35
+#define dio2_lora1 21
+#define dio2_lora2 17
 
-int counter = 0;
+
+// Swap which line is commented to switch which module is used
+SX1262 lora = new Module(ss_lora1, dio2_lora1, rst, busy_lora1);
+//SX1262 lora = new Module(ss_lora2, dio2_lora2, rst, busy_lora2);
 
 void setup() {
-
   Serial.begin(115200);
-
-  Serial.println("LoRa Sender");
 
   pinMode(ss_lora1, OUTPUT);
   pinMode(ss_lora2, OUTPUT);
-  digitalWrite(ss_lora1, LOW);
-  digitalWrite(ss_lora2, LOW);
+  digitalWrite(ss_lora1, HIGH);
+  digitalWrite(ss_lora2, HIGH);
 
-  LoRa.setPins(ss_lora1, rst);
-  
-  while (!LoRa.begin(915E6)) {
-    Serial.println(".");
-    delay(500);
+  // initialize SX1262 with default settings
+  Serial.print(F("[SX1262] Initializing ... "));
+  // carrier frequency:           434.0 MHz
+  // bandwidth:                   125.0 kHz
+  // spreading factor:            9
+  // coding rate:                 7
+  // sync word:                   0x12 (private network)
+  // output power:                14 dBm
+  // current limit:               60 mA
+  // preamble length:             8 symbols
+  // TCXO voltage:                1.6 V (set to 0 to not use TCXO)
+  // regulator:                   DC-DC (set to true to use LDO)
+  // CRC:                         enabled
+  int state = lora.begin();
+  if (state == ERR_NONE) {
+    Serial.println(F("success!"));
+  } else {
+    Serial.print(F("failed, code "));
+    Serial.println(state);
+    while (true);
+  }
+  // set carrier frequency to 433.5 MHz
+  if (lora.setFrequency(915.0) == ERR_INVALID_FREQUENCY) {
+    Serial.println(F("Selected frequency is invalid for this module!"));
+    while (true);
   }
   
-  // Change sync word (0xF3) to match the receiver
-  // The sync word assures you don't get LoRa messages from other LoRa transceivers
-  // ranges from 0-0xFF
-  LoRa.setSyncWord(0xF3);
-  Serial.println("LoRa Initializing OK!");
 }
 
 void loop() {
-  Serial.print("Sending packet: ");
-  Serial.println(counter);
+  Serial.print(F("[SX1262] Transmitting packet ... "));
 
-  LoRa.beginPacket();
-  LoRa.print("hello ");
-  LoRa.print(counter);
-  LoRa.endPacket();
+  // you can transmit C-string or Arduino string up to
+  // 256 characters long
+  // NOTE: transmit() is a blocking method!
+  //       See example SX126x_Transmit_Interrupt for details
+  //       on non-blocking transmission method.
+  int state = lora.transmit("Hello World!");
 
-  counter++;
+  // you can also transmit byte array up to 256 bytes long
+  /*
+    byte byteArr[] = {0x01, 0x23, 0x45, 0x56, 0x78, 0xAB, 0xCD, 0xEF};
+    int state = lora.transmit(byteArr, 8);
+  */
 
-  delay(10000);
+  if (state == ERR_NONE) {
+    // the packet was successfully transmitted
+    Serial.println(F("success!"));
+
+    // print measured data rate
+    Serial.print(F("[SX1262] Datarate:\t"));
+    Serial.print(lora.getDataRate());
+    Serial.println(F(" bps"));
+
+  } else if (state == ERR_PACKET_TOO_LONG) {
+    // the supplied packet was longer than 256 bytes
+    Serial.println(F("too long!"));
+
+  } else if (state == ERR_TX_TIMEOUT) {
+    // timeout occured while transmitting packet
+    Serial.println(F("timeout!"));
+
+  } else {
+    // some other error occurred
+    Serial.print(F("failed, code "));
+    Serial.println(state);
+
+  }
+
+  // wait for a second before transmitting again
+  delay(1000);
 }
